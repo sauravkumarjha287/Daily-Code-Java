7eada642ef17bd0e8dc1279bacf93235cfdf3ee4
sauravkumarjha287 <124920386+sauravkumarjha287@users.noreply.github.com>
2025-04-22 00:51:43 +0530

    Add files via upload

---
+++ ThreadDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.daythirteen;
+
+public class ThreadDemo {
+   
+	public static void main(String[] args) {
+		
+		Thread t=Thread.currentThread();
+		System.out.println(t.getName());
+		System.out.println(t);
+		System.out.println("-------------------------");
+		
+		
+	}
+	
+
+}
